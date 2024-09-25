e58f85952ed1adcacdb66cc0550ccbf2f7d9c955
mashaler <mashalerolva@gmail.com>
2024-09-26 01:05:30 +0200

    added changes

--- QR Code Generator/advanceQrGenerator.py
+++ QR Code Generator/advanceQrGenerator.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Author: Aman Kumar
 # Version: 1.0.0 
 
 import qrcode
@@ -8,7 +7,7 @@ from PIL import Image
 
 qr = qrcode.QRCode(version=1, error_correction=qrcode.ERROR_CORRECT_H, box_size=10, border=4)
 
-qr.add_data("https://github.com/king04aman/all-in-one-python-projects")
+qr.add_data("https://mashaler.github.io/personal_portfolio-website.github.io/")
 qr.make(fit=True)
 img = qr.make_image(fill_color='blue', back_color='white')
 img.save('./advanceQR.png')
